817df7b826e176a2eb67e3b400e2279e1a9c1ec5
Reynald231213 <reynaldsilva123@gmail.com>
2020-09-16 00:31:23 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';  
+import './App.css';
+import Harga from './Components/Harga'
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <Harga />
+            </div>
+        );
+    }
+}
+export default App
